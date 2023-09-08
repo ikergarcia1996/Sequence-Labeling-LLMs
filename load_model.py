066ebbed7cd469d63584f79d45d1e96973783c34
ikergarciaf <igarciaf896@gmail.com>
2023-09-08 13:24:34 +0200

    Bfloat16 default

--- load_model.py
+++ load_model.py
@@ -291,7 +291,7 @@ def load_model_for_training(
                 load_in_4bit=True,
                 bnb_4bit_use_double_quant=True,
                 bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch.float16
+                bnb_4bit_compute_dtype=torch.bloat16
                 if torch_dtype in ["auto", None]
                 else torch_dtype,
             )
