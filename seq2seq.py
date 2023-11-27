b568906acbd8dbb4c5f13ae2ddd48e3be757b6da
ikergarciaf <igarciaf896@gmail.com>
2023-11-27 23:58:16 +0100

    Set kwargs

--- seq2seq.py
+++ seq2seq.py
@@ -933,13 +933,22 @@ def seq2seq(
         elif optim.lower() == "deepspeed":
             from accelerate.utils import DummyOptim
 
+            kwargs = {}
+            kwargs["optimizer"]["params"]["betas"] = (0.9, 0.995)
+            kwargs["optimizer"]["params"]["lr"] = learning_rate
+            kwargs["optimizer"]["params"]["eps"] = 1e-7
+            kwargs["optimizer"]["params"]["weight_decay"] = weight_decay
+
+            kwargs["scheduler"]["params"]["warmup_min_lr"] = 0
+            kwargs["scheduler"]["params"]["warmup_max_lr"] = learning_rate
+            kwargs["scheduler"]["params"]["warmup_num_steps"] = num_warmup_steps
+            kwargs["scheduler"]["params"]["warmup_type"] = "linear"
+            kwargs["scheduler"]["params"]["total_num_steps"] = max_train_steps
+
             optimizer = DummyOptim(
                 params=optimizer_grouped_parameters,
                 lr=learning_rate,
                 weight_decay=weight_decay,
-                kwargs=dict(
-                    betas=(0.9, 0.995),
-                ),
             )
         else:
             raise ValueError(
